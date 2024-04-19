4aade691a0766a95e5eadfed40bfeaa4ccacc714
yukianime <163084032+yukianime@users.noreply.github.com>
2024-04-19 00:34:41 -0300

    version 0,6
    Fuera columnas, más animes

--- carrusel.js
+++ carrusel.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function() {
     var carousel = document.getElementById('carouselExampleFade');
     var iframes = carousel.querySelectorAll('iframe');
@@ -8,4 +9,16 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   });
+=======
+document.addEventListener("DOMContentLoaded", function() {
+    var carousel = document.getElementById('carouselExampleFade');
+    var iframes = carousel.querySelectorAll('iframe');
+  
+    carousel.addEventListener('slide.bs.carousel', function() {
+      for(var i = 0; i < iframes.length; i++) {
+        iframes[i].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+      }
+    });
+  });
+>>>>>>> d8a593e77eb629b97c797cdeabd42ed199d448cd
   
